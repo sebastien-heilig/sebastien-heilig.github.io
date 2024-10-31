a8e1cfa3c27ea983d3df96d3b2994ab709f8da74
sebastien-heilig <sebastien.heilig@gmail.com>
2024-10-31 09:55:04 +0100

    Move the content at root level

--- server/en/main.server.mjs
+++
@@ -1,3 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-LZLTQJE2.mjs";import"./chunk-NJVBKL2R.mjs";import"./chunk-E2BLNY6D.mjs";export{a as default};
-/**i18n:92cadf241b56b324fa80c0d90c1d0d734ebc8170787c5da8eb634f2fba4720f9*/
